96adbedeb6434da4c37d7918f0f4afc014a929f9
Yuri Feldman <arrayjam@tbqh.net>
2014-06-30 15:50:10 +1000

    Update lib

--- javascripts/fabulous.js
+++ javascripts/fabulous.js
@@ -5,5 +5,5 @@ $(function() {
   var replacers = title.text().trim().split("").map(function(letter) { return $("<span>").text(letter); });
   title.html(replacers);
   $(".whee").on("click", function() { window.getSelection().selectAllChildren(document); });
-  $("section").fabulous(10);
+  $("section").fabulous({style: "cubehelix-rainbow", cycle: 50, rotation: 20, preview: true});
 });
