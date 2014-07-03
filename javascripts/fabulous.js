56ba0ce0810254fed04a95ef8f56b2cc905fb84f
Yuri Feldman <arrayjam@tbqh.net>
2014-07-03 11:51:02 +1000

    Start freshhhhhhhhhh

--- javascripts/fabulous.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-  var title = $(".split-and-fabulousize");
-
-  var replacers = title.text().trim().split("").map(function(letter) { return $("<span>").text(letter); });
-  title.html(replacers);
-  $(".whee").on("click", function() { window.getSelection().selectAllChildren(document); });
-  $("section").fabulous({style: "cubehelix-rainbow", cycle: 50, rotation: 20, preview: true});
-});
