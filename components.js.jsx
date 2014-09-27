1e1722f59aa4c150993dc0c4064c910b8f620129
Yuri Feldman <yuri@tbqh.net>
2014-09-27 23:35:40 +1000

    Single eval path

--- components.js.jsx
+++ components.js.jsx
@@ -3,22 +3,27 @@
 // Allow editing of code example and evaluate
 var OptionsExample = React.createClass({
   getInitialState: function() {
-    return { optionsHash: "" };
+    return { optionsHash: this.props.documentation.options[0].code };
   },
 
-  handleOptionSelect: function(optionsHash) {
+  handleCodeChange: function(optionsHash) {
     this.setState({optionsHash: optionsHash});
+    this.evaluate();
+  },
+
+  evaluate: function() {
+    console.log("EVAL", arguments);
   },
 
   render: function() {
     var options = this.props.documentation.options.map(function(option) {
       return (
-        <Option optionName={option.name} optionHash={option.code} onOptionSelect={this.handleOptionSelect} />
+        <OptionsExampleOption optionName={option.name} optionHash={option.code} onOptionSelect={this.handleCodeChange} />
       );
     }.bind(this));
     return (
       <div>
-        $("#{this.props.documentation.name}").fabulous({"{ "}<span contentEditable>{this.state.optionsHash}</span>{" }"});
+        <OptionsExampleEvaluator selector={"#" + this.props.documentation.name} optionsHash={this.state.optionsHash} onCodeChange={this.handleCodeChange} />
         <ul>
           {options}
         </ul>
@@ -28,7 +33,20 @@ var OptionsExample = React.createClass({
   }
 });
 
-var Option = React.createClass({
+var OptionsExampleEvaluator = React.createClass({
+  render: function() {
+    return (
+      <div>$("{this.props.selector}").fabulous({"{ "}<span contentEditable onInput={this.handleInput}>{this.props.optionsHash}</span>{" }"});</div>
+    );
+  },
+
+  handleInput: function(event) {
+    this.props.onCodeChange(event.target.innerHTML);
+  }
+
+});
+
+var OptionsExampleOption = React.createClass({
   handleClick: function() {
     this.props.onOptionSelect(this.props.optionHash);
   },
